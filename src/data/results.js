502f9b312c391d81f0b3842c8b7dc8ef41ef87f8
naraekn <34229459+naraekn@users.noreply.github.com>
2020-08-16 00:44:26 +0900

    Real results (incomplete)

--- src/data/results.js
+++ src/data/results.js
@@ -1,18 +1,48 @@
 const results = [
   {
     id: 1,
-    name: 'Facebook',
-    description: '페이스북은 ~',
+    name: 'Google',
+    description: '구글은 ~',
   },
   {
     id: 2,
+    name: 'Netflix',
+    description: '넷플릭스는~',
+  },
+  {
+    id: 3,
+    name: 'Facebook',
+    description: '페이스북은~',
+  },
+  {
+    id: 4,
+    name: 'Plug and Play',
+    description: '플러그앤플레이는 세계에서 가장 큰 스타트업 인큐베이팅 센터로~',
+  },
+  {
+    id: 5,
+    name: 'Amazon',
+    description: '아마존~',
+  },
+  {
+    id: 6,
+    name: 'Intel',
+    description: '인텔~',
+  },
+  {
+    id: 7,
+    name: 'Apple',
+    description: '애플은~',
+  },
+  {
+    id: 8,
     name: 'Tesla',
     description: '테슬라는~',
   },
   {
-    id: 3,
-    name: 'Amazon',
-    description: '아마존은~',
+    id: 9,
+    name: 'Microsoft',
+    description: '마이크로소프트는~',
   },
 ];
 
