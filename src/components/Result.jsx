912a743eabddd6f7eaf4fc3f56ed28961ea5e622
naraekn <34229459+naraekn@users.noreply.github.com>
2020-08-19 16:31:52 +0900

    Result Page Design
    Apply design to ResultPage with sample data.
    This work is in progress and includes
    temporary codes to take the next step.

---
+++ src/components/Result.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+import { RESULT_GRADIENT_COLORS } from '../styles/constants';
+
+import ProfileBar from './result/ProfileBar';
+import ResultContent from './result/ResultContent';
+import CompanyTitle from './result/CompanyTitle';
+import ResultPageButtons from './result/ResultPageButtons';
+
+const Container = styled.div(({ id }) => ({
+  height: '100%',
+  backgroundImage: `linear-gradient(
+    60deg, 
+    ${RESULT_GRADIENT_COLORS[2][0]}, 
+    ${RESULT_GRADIENT_COLORS[2][1]}
+  )`,
+}));
+
+// TODO: Props will be applied after the ResultPage design is fixed
+
+export default function Result({ first, second, last }) {
+  return (
+    <Container>
+      <ProfileBar />
+      <CompanyTitle />
+      <ResultContent />
+      <ResultPageButtons />
+    </Container>
+  );
+}
