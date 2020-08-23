f19994866c0cc30e2c14f9d0bdc7e3802eadc0ff
naraekn <34229459+naraekn@users.noreply.github.com>
2020-08-23 18:37:27 +0900

    Remove unused codes

--- src/containers/ResultContainer.jsx
+++ src/containers/ResultContainer.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 
-// import { useSelector } from 'react-redux';
-
-// TODO: Remove this code after modifying design
-import results from '../data/results';
+import { useSelector } from 'react-redux';
 
 import Result from '../components/Result';
 
-// import { get } from '../utils';
+import { get } from '../utils';
 
 export default function ResultContainer({ handleClickLink }) {
-  // TODO: Remove this comment after modifying design
-  // const { first, second, last } = useSelector(get('result'));
-
-  // TODO: Remove this code after modifying design
-  const first = results.find(({ id }) => id === 9);
-  const second = results.find(({ id }) => id === 3);
-  const last = results.find(({ id }) => id === 7);
+  const { first, second, last } = useSelector(get('result'));
 
   return (
     <>
